778c66def48dcfe040b596ffe0028fd30fdee8ad
wangchuktandin <wangchuktandin@bnb.bt>
2025-08-19 16:51:59 +0600

    Update list-users.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+###########
+# About: 
+# Input: 
+#
+# Owner:
+#
+#########
+
+helper()
+
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -36,6 +46,12 @@ function list_users_with_read_access {
     fi
 }
 
+function helper {
+expected_cmd_args=2
+if [ $# -ne $expected_cmd_args]; then
+    echo "Please execute the script with required cmd args"
+    echo "asd
+}
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
